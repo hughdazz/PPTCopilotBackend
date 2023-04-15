ffd9ea780dffae4cb13c033dad5db38d64d983f4
nekotoxin <nekotoxin@gmail.com>
2023-04-15 14:38:56 +0800

    gpt service： 1. 新增gpt config 2. 密钥设置读取

--- models/init.go
+++ models/init.go
@@ -5,7 +5,7 @@ import "github.com/beego/beego/v2/client/orm"
 // 初始化数据表
 func init() {
 	// 设置默认数据库
-	orm.RegisterDataBase("default", "mysql", "root:@tcp(localhost:3306)/now_db?charset=utf8&loc=Local")
+	orm.RegisterDataBase("default", "mysql", "root:admin@tcp(host.docker.internal:3307)/now_db?charset=utf8&loc=Local")
 	// 注册定义的model
 	orm.RegisterModel(new(User))
 	orm.RegisterModel(new(Project))
