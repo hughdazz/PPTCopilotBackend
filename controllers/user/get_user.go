43ca7d439ea3957df6e5fd43a18b15b830b1e368
hughdazz <hughdazz280@163.com>
2023-05-05 05:25:27 +0000

    add get_user

---
+++ controllers/user/get_user.go
@@ -0,0 +1,26 @@
+package user
+
+import (
+	"backend/controllers"
+	"backend/models"
+	"strconv"
+)
+
+func (this *Controller) GetUser() {
+	// 获取路由参数
+	id_ := this.Ctx.Input.Param(":id")
+	id, err := strconv.Atoi(id_)
+	if err != nil {
+		this.Data["json"] = controllers.MakeResponse(401, err.Error(), nil)
+		this.ServeJSON()
+		return
+	}
+	user, err := models.GetUser(id)
+	if err != nil {
+		this.Data["json"] = controllers.MakeResponse(401, err.Error(), nil)
+		this.ServeJSON()
+		return
+	}
+	this.Data["json"] = controllers.MakeResponse(200, "success", user)
+	this.ServeJSON()
+}
