ebb685dd47df4a18274c9bda4c6d89ff4b63689f
hughdazz <hughdazz280@163.com>
2023-05-09 15:03:19 +0000

    fix

--- routers/cors.go
+++ routers/cors.go
@@ -5,7 +5,8 @@ import (
 )
 
 func cors_access(context *context.Context) {
-	context.ResponseWriter.Header().Set("Access-Control-Allow-Origin", "http://localhost:9529")
+	// 9529+7777
+	context.ResponseWriter.Header().Set("Access-Control-Allow-Origin", "http://localhost:7777;http://localhost:9529")
 	context.ResponseWriter.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	context.ResponseWriter.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, token")
 	context.ResponseWriter.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -15,17 +16,17 @@ func cors_access(context *context.Context) {
 		context.ResponseWriter.WriteHeader(200)
 		return
 	}
-	// 如果访问static目录下文件，直接返回
-	if context.Request.RequestURI[:7] == "/static" {
-		return
-	}
+	// // 如果访问static目录下文件，直接返回
+	// if context.Request.RequestURI[:7] == "/static" {
+	// 	return
+	// }
 
-	if context.Request.RequestURI == "/user/login" || context.Request.RequestURI == "/initial" {
-		return
-	}
-	_, err := context.Request.Cookie("token")
-	if err != nil {
-		context.ResponseWriter.WriteHeader(401)
-		return
-	}
+	// if context.Request.RequestURI == "/user/login" || context.Request.RequestURI == "/initial" {
+	// 	return
+	// }
+	// _, err := context.Request.Cookie("token")
+	// if err != nil {
+	// 	context.ResponseWriter.WriteHeader(401)
+	// 	return
+	// }
 }
