95bcdc9133a299626b24af432796ba00a674945b
hughdazz <hughdazz280@163.com>
2023-05-27 03:31:02 +0000

    .

--- controllers/gpt/apikey_pool.go
+++ controllers/gpt/apikey_pool.go
@@ -3,6 +3,7 @@ package gpt
 import (
 	"backend/conf"
 	"fmt"
+	"math/rand"
 )
 
 type ApiKeyPool struct {
@@ -21,12 +22,20 @@ func GetApiKey() (string, error) {
 	mutex <- true
 	defer func() { <-mutex }()
 
+	// 随机选择一个可用ApiKey
+	var validKeys []string
 	for _, key := range pool.ApiKeyList {
 		if !pool.InUse[key] {
-			pool.InUse[key] = true
-			return key, nil
+			validKeys = append(validKeys, key)
 		}
 	}
+	if len(validKeys) > 0 {
+		// 生成随机数
+		randIndex := rand.Intn(len(validKeys))
+		pool.InUse[validKeys[randIndex]] = true
+		return validKeys[randIndex], nil
+	}
+
 	return "", fmt.Errorf("no api key available")
 }
 
