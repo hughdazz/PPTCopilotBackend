79cbc4a1f43976624a3bb1055313024e05003b9a
Gershwin <1621605907@qq.com>
2023-05-28 16:10:33 +0000

    fix static inital

--- controllers/static.go
+++ controllers/static.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
-	beego "github.com/beego/beego/v2/server/web"
 	"io"
 	"net/http"
 	"os"
 	"strings"
+
+	beego "github.com/beego/beego/v2/server/web"
 )
 
 type StaticRouter struct {
@@ -18,14 +19,19 @@ func (c *StaticRouter) Get() {
 	url := c.Ctx.Request.URL.Path
 	// & /_static/user/:id/avatar.png
 	url = url[2:]
+	_, err := os.Stat(url)
+	if err == nil {
+		http.ServeFile(c.Ctx.ResponseWriter, c.Ctx.Request, url)
+		return
+	}
 	if strings.Contains(url, "user/") && strings.Contains(url, "avatar.png") {
 		http.ServeFile(c.Ctx.ResponseWriter, c.Ctx.Request, "static/user/default.png")
 		return
 	}
-	//if strings.Contains(url, "project/") && strings.Contains(url, "cover.png") {
-	//	http.ServeFile(c.Ctx.ResponseWriter, c.Ctx.Request, "static/project/default.png")
-	//	return
-	//}
+	if strings.Contains(url, "project/") && strings.Contains(url, "cover.png") {
+		http.ServeFile(c.Ctx.ResponseWriter, c.Ctx.Request, "static/project/default.png")
+		return
+	}
 	http.ServeFile(c.Ctx.ResponseWriter, c.Ctx.Request, url)
 }
 
